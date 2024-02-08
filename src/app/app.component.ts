0229ea12671f65cabb165411e28556d50517ab74
Iraj Mashar <lrinchan.kagamine@gmail.com>
2024-02-08 23:25:10 +0100

    finished themeing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { ThemeService } from './theme.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'baby-steps';
+  isMale: boolean;
+
+  constructor(private themeService: ThemeService){}
+
+  ngOnInit(): void {
+    this.themeService.isMale$.subscribe(value => this.isMale = value);
+  }
+
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { ThemeService } from './../theme.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+
+  isMale: boolean;
+
+  constructor(private themeService: ThemeService){}
+
+  ngOnInit(): void {
+    this.themeService.isMale$.subscribe(value => this.isMale = value);
+  }
 
 }
