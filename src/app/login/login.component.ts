7385104af8a967097985963cb8c0aa04be2c4d94
Iraj Mashar <i.mashar@uniccomp.de>
2024-02-14 17:21:57 +0100

    added styling to login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { ThemeService } from './../theme.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  isMale: boolean;
+
+  constructor(private themeService: ThemeService){
+
+  }
+
+  ngOnInit(): void {
+      this.themeService.isMale$.subscribe(value => this.isMale = value);
+  }
 
   debug(x){
     console.log(x);
   }
+
 
 }
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '../theme.service';
 
 @Component({
   selector: 'app-signup-form',
   templateUrl: './signup-form.component.html',
   styleUrls: ['./signup-form.component.css']
 })
-export class SignupFormComponent {
+export class SignupFormComponent implements OnInit{
+
+  isMale: boolean;
+
+  constructor(private themeSerivce: ThemeService) {
+    
+  }
+
+  ngOnInit(): void {
+      this.themeSerivce.isMale$.subscribe(value => this.isMale = value);
+  }
+  
 
 }
